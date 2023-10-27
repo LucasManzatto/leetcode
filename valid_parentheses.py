7862ec0caa69a1d582463fe9e69a6d3d65e69360
Lucas Manzatto Peres <99817226@ambev.com.br>
2023-10-27 14:13:34 -0300

    add solution

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,68 +1,49 @@
-# 20. Valid Parentheses
+class Solution:
+    def check_valid(self, s: str) -> bool:
+        return (
+            len(s) % 2 == 0
+            and (s.count("(") + s.count(")")) % 2 == 0
+            and (s.count("{") + s.count("}")) % 2 == 0
+            and (s.count("[") + s.count("]")) % 2 == 0
+            and s.count("(") - s.count(")") == 0
+            and s.count("{") - s.count("}") == 0
+            and s.count("[") - s.count("]") == 0
+        )
 
-# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    def find_closing_pair(self, s, opening, valids, seen, start_index):
+        last_seen_index = None
+        for index, closing in enumerate(s):
+            if index < start_index:
+                continue
+            if valids[opening] == closing and index not in seen:
+                last_seen_index = index
+        return last_seen_index
 
-# An input string is valid if:
-
-# Open brackets must be closed by the same type of brackets.
-# Open brackets must be closed in the correct order.
-# Every close bracket has a corresponding open bracket of the same type.
-
-
-# Example 1:
-
-# Input: s = "()"
-# Output: true
-# Example 2:
-
-# Input: s = "()[]{}"
-# Output: true
-# Example 3:
-
-
-# Input: s = "(]"
-# Output: false
-
-
-def check_valid(s: str) -> bool:
-    return (
-        len(s) % 2 == 0
-        and (s.count("(") + s.count(")")) % 2 == 0
-        and (s.count("{") + s.count("}")) % 2 == 0
-        and (s.count("[") + s.count("]")) % 2 == 0
-    )
-
-
-def find_closing_pair(s, opening, valids, seen):
-    last_seen_index = None
-    for index, closing in enumerate(s):
-        if valids[opening] == closing and index not in seen:
-            last_seen_index = index
-    return last_seen_index
-
-
-def isValid(s: str, valids) -> bool:
-    if not check_valid(s):
-        return False
-    seen = []
-    pairs = []
-    for index, c in enumerate(s):
-        if index in seen:
-            continue
-        if c in valids.keys():
-            seen.append(index)
-            closing_pair_index = find_closing_pair(
-                s=s, opening=c, valids=valids, seen=seen
-            )
-            if not closing_pair_index:
-                return False
-            if not check_valid(s[index : closing_pair_index + 1]):
-                return False
-            seen.append(closing_pair_index)
-    return True
+    def isValid(self, s: str) -> bool:
+        valids = {"(": ")", "{": "}", "[": "]"}
+        if not self.check_valid(s):
+            return False
+        seen = []
+        pairs = []
+        for index, c in enumerate(s):
+            print(index,c,seen)
+            if index in seen:
+                continue
+            if c in valids.keys():
+                print(c)
+                seen.append(index)
+                closing_pair_index = self.find_closing_pair(
+                    s=s, opening=c, valids=valids, seen=seen, start_index=index
+                )
+                print(index, closing_pair_index)
+                print(s[index : closing_pair_index + 1])
+                if not closing_pair_index:
+                    return False
+                if not self.check_valid(s[index : closing_pair_index + 1]):
+                    return False
+                seen.append(closing_pair_index)
+        return True
 
 
-s = "(())[]{}"
-valids = {"(": ")", "{": "}", "[": "]"}
-
-result = isValid(s=s, valids=valids)
+s = "({(())}[()])"
+valid = Solution().isValid(s=s)
