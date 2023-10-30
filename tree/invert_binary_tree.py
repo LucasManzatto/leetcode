fe16cc1b0c42a9685e1ab4e00a6cbab8af6909c8
Lucas <99817226@ambev.com.br>
2023-10-30 17:24:49 -0300

    add solution

---
+++ tree/invert_binary_tree.py
@@ -0,0 +1,48 @@
+'''
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+'''
+
+from binary_tree import TreeNode, post_order_transversal, pre_order_traversal, reverse_tree, in_order_traversal, visualize_binary_tree
+from typing import Optional
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+        aux = root.left
+        root.left = root.right
+        root.right = aux
+        self.invertTree(root=root.left)
+        self.invertTree(root=root.right)
+        return root
+    
+root = TreeNode(val=4)
+root.left = TreeNode(val=2)
+root.left.left = TreeNode(val=1)
+root.left.right = TreeNode(val=3)
+
+root.right = TreeNode(val=7)
+root.right.left = TreeNode(val=6)
+root.right.right = TreeNode(val=9)
+
+new_root = reverse_tree(root)
+
+# print(Solution().invertTree(root=root))
