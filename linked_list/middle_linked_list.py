fe189b4626c4f2d95b8b345cc73b023ac27269df
Lucas <99817226@ambev.com.br>
2023-11-29 09:54:14 -0300

    update description

--- linked_list/middle_linked_list.py
+++ linked_list/middle_linked_list.py
@@ -1,5 +1,7 @@
 """
-Given the head of a singly linked list, reverse the list, and return the reversed list.
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
 
  
 
@@ -7,25 +9,20 @@ Example 1:
 
 
 Input: head = [1,2,3,4,5]
-Output: [5,4,3,2,1]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
 Example 2:
 
 
-Input: head = [1,2]
-Output: [2,1]
-Example 3:
-
-Input: head = []
-Output: []
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
  
 
 Constraints:
 
-The number of nodes in the list is the range [0, 5000].
--5000 <= Node.val <= 5000
- 
-
-Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
 """
 import os, sys
 
