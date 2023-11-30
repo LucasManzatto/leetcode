83153799843f05efffe710ccb5085a64567f2423
Lucas <99817226@ambev.com.br>
2023-11-30 10:31:23 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="leetcode",
+    version="1.0",
+    description="Leetcode problems with data strcutres",
+    author="Lucas",
+    author_email="",
+    packages=find_packages(exclude=('tests', 'docs'))
+)
