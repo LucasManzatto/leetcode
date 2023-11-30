75a38dc969500b5d4b93ca86cd8d02fad5eb63c0
Lucas <99817226@ambev.com.br>
2023-11-30 10:33:34 -0300

    fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="leetcode",
     version="1.0",
-    description="Leetcode problems with data strcutres",
+    description="Leetcode problems with data structures",
     author="Lucas",
     author_email="",
     packages=find_packages(exclude=('tests', 'docs'))
